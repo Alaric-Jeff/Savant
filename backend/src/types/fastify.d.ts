727373ef2ab18162d1eba69b5b2d4b3367d980ab
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-10 18:04:43 +0800

    modified module for type safety and augmentation

--- backend/src/types/fastify.d.ts
+++ backend/src/types/fastify.d.ts
@@ -1,9 +1,13 @@
-// types/fastify.d.ts
 import 'fastify';
 import { PrismaClient } from '@prisma/client';
+import signToken from '@services/tokenServices/signToken.ts';
+import verifyToken from '@services/tokenServices/verifyToken.ts';
+import { JwtPayload } from 'jsonwebtoken';
 
 declare module 'fastify' {
   interface FastifyInstance {
     prisma: PrismaClient;
+    signToken: (id: number) => string;
+    verifyToken: (token: string) => JwtPayload | string;
   }
 }
