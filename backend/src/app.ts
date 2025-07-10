d3a4116e2803016beb47f36f55f22ca7bbc58124
Alaric-Jeff <jefsohandsome1@gmail.com>
2025-07-10 18:06:04 +0800

    registered plugins

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import prismaPlugin from "./plugins/prismaPlugin.js";
+import jwtPlugin from "@plugins/jwtPlugin.js";
 import compression from '@fastify/compress'
 import dotenv from 'dotenv'
 
@@ -8,29 +9,30 @@ const fastify = Fastify({
     logger: true
 })
 
-fastify.register(prismaPlugin)
+fastify.register(prismaPlugin);
+fastify.register(jwtPlugin);
 fastify.register(compression, {
     threshold: 1024
-})
+});
 
 const http_port = process.env.http_port? Number(process.env.http_port) : 3000;
-const host = process.env.host? String(process.env.host) : "127.0.0.1"
+const host = process.env.host? String(process.env.host) : "127.0.0.1";
 
 try{
     fastify.listen({
         port: http_port,
         host: host
     }).then(()=> {
-        fastify.log.info(`Server is running on server ${http_port}, hosted with: ${host}`)
-    })
+        fastify.log.info(`Server is running on server ${http_port}, hosted with: ${host}`);
+    });
 }catch(err: unknown){
     if(err instanceof Error){
-        fastify.log.error(`Error occured in starting the server, error: ${err.name}, message: ${err.message}, stack: ${err.stack}`)
+        fastify.log.error(`Error occured in starting the server, error: ${err.name}, message: ${err.message}, stack: ${err.stack}`);
     }else{
-        fastify.log.error(`Unknown error occured in starting the server: ${err}`)
+        fastify.log.error(`Unknown error occured in starting the server: ${err}`);
     }
     fastify.close().then(()=> {
-        process.exit(1)
+        process.exit(1);
     });
 }
 
